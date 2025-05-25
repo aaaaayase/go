fc6b76b7cdd14332d6399de778b456c926770534
aaaaayase <2284858868@qq.com>
2025-05-25 23:49:44 +0800

    first commit

---
+++ firstGolang/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// main函数
+
+func main() {
+	fmt.Println("hello go!")
+	time.Sleep(time.Second * 1)
+}
---
+++ var/test1.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// 声明全局变量 第四种方法不可以
+var aG = 100
+
+func main() {
+	var a int
+	fmt.Println(a)
+
+	var b int = 100
+	fmt.Println(b)
+
+	var c = 100
+	fmt.Println(c)
+	fmt.Printf("%T\n", c)
+
+	var d string = "111"
+	fmt.Printf("%T\n", d)
+
+	e := 100
+	fmt.Println(e)
+	fmt.Printf("%T\n", e)
+
+	fmt.Println(aG)
+
+}
